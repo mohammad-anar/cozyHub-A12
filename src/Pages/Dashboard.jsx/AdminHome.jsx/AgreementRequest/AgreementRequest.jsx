758d9de7e0e137364d934335756d44c02c0628ca
Mohammad Anar <ai.anarul4232@gmail.com>
2023-11-30 16:40:23 +0600

    manage cupon added

--- src/Pages/Dashboard.jsx/AdminHome.jsx/AgreementRequest/AgreementRequest.jsx
+++ src/Pages/Dashboard.jsx/AdminHome.jsx/AgreementRequest/AgreementRequest.jsx
@@ -1,9 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../../Hooks/useAxiosSecure/axiosSecure";
+import toast from "react-hot-toast";
+import { FallingLines } from "react-loader-spinner";
 
 const AgreementRequest = () => {
   const axiosSecure = useAxiosSecure();
-  const { data:agreements } = useQuery({
+  const {
+    data: agreements,
+    refetch,
+    isloading,
+  } = useQuery({
     queryKey: ["agreements"],
     queryFn: async () => {
       const res = await axiosSecure.get("/agreements");
@@ -11,45 +17,98 @@ const AgreementRequest = () => {
     },
   });
   console.log(agreements?.data);
+  const handleAccept = (id) => {
+    axiosSecure
+      .put(`agreements/${id}`)
+      .then((res) => {
+        console.log(res.data);
+        toast.success("status updated");
+      })
+      .catch((err) => {
+        toast.error(err.message);
+      });
+    axiosSecure.put(`users/${id}`).then((res) => {
+      console.log(res.data);
+      toast.success("user updated");
+    });
+  };
+  const handleReject = (id) => {
+    axiosSecure
+      .delete(`/agreements/${id}`)
+      .then((res) => {
+        console.log(res.data);
+        toast.success("rejected");
+        refetch();
+      })
+      .catch((err) => {
+        console.log(err);
+        toast.error(err.message);
+      });
+  };
   return (
     <div className="p-2 md:p-4 lg:p-12 bg-white m-2 md:m-4 lg:m-0 lg:mt-6 w-[350px] min-w-full xs:w-full sm:max-w-full md:max-w-full mx-auto">
       <h2 className="text-2xl font-medium mb-6">Agreements Requests</h2>
-      <div>
-        <div className="overflow-x-scroll">
-          <table className="table table-xs z-0 w-full table-pin-rows table-pin-cols">
-            <thead>
-              <tr className="overflow-x-scroll">
-                <th>#</th>
-                <td>User Name</td>
-                <td>Eamil</td>
-                <td>Floor no</td>
-                <td>Block name</td>
-                <td>Room no</td>
-                <td>Rent</td>
-                <th>Req date</th>
-                <th>Action</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                agreements?.data?.map((agreement, indx) => <tr key={agreement._id}>
-                <th>{indx + 1}</th>
-                <td>{agreement?.userName}</td>
-                <td>{agreement?.userEmail}</td>
-                <td>{agreement?.floor_no}</td>
-                <td>{agreement?.block_name}</td>
-                <td>{agreement?.apartment_no}</td>
-                <td>{agreement?.rent}</td>
-                <td>{agreement?.createdAt.slice(4, 22)}</td>
-                <td><button className="btn btn-sm bg-green-600 text-white">Accept</button></td>
-                <td><button className="btn btn-sm bg-red-600 text-white">Reject</button></td>
-              </tr>)
-              }
-            </tbody>
-          </table>
+      {isloading ? (
+        <div className="h-screen flex items-center justify-center">
+          <FallingLines
+            color="#4834d4"
+            width="100"
+            visible={true}
+            ariaLabel="falling-lines-loading"
+          />
         </div>
-      </div>
+      ) : (
+        <div>
+          <div className="overflow-x-scroll">
+            <table className="table table-xs z-0 w-full table-pin-rows table-pin-cols">
+              <thead>
+                <tr className="overflow-x-scroll">
+                  <th>#</th>
+                  <td>User Name</td>
+                  <td>Eamil</td>
+                  <td>Floor no</td>
+                  <td>Block name</td>
+                  <td>Room no</td>
+                  <td>Rent</td>
+                  <th>Req date</th>
+                  <th>Action</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {agreements?.data?.map((agreement, indx) => (
+                  <tr key={agreement._id}>
+                    <th>{indx + 1}</th>
+                    <td>{agreement?.userName}</td>
+                    <td>{agreement?.userEmail}</td>
+                    <td>{agreement?.floor_no}</td>
+                    <td>{agreement?.block_name}</td>
+                    <td>{agreement?.apartment_no}</td>
+                    <td>{agreement?.rent}</td>
+                    <td>{agreement?.createdAt.slice(4, 22)}</td>
+                    <td>
+                      <button
+                        onClick={() => handleAccept(agreement?._id)}
+                        className="btn btn-sm bg-green-600 text-white"
+                      >
+                        Accept
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        onClick={() => handleReject(agreement?._id)}
+                        className="btn btn-sm bg-red-600 text-white"
+                      >
+                        Reject
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/Pages/Dashboard.jsx/AdminHome.jsx/ManageCupons/ManageCupons.jsx
+++ src/Pages/Dashboard.jsx/AdminHome.jsx/ManageCupons/ManageCupons.jsx
@@ -1,10 +1,60 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../../Hooks/useAxiosSecure/axiosSecure";
 
 const ManageCupons = () => {
-    return (
-        <div>
-            Manage Cupons
-        </div>
-    );
+  const axiosSecure = useAxiosSecure();
+  const {
+    data: cupons,
+    isloading,
+    refetch,
+  } = useQuery({
+    queryKey: ["cupons"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/cupons");
+      return res;
+    },
+  });
+  console.log(cupons?.data);
+  return (
+    <div className="m-2 mt-8">
+      <h2 className="text-xl font-semibold mb-4 ml-2">Manage Cupons</h2>
+      <div className="p-2 md:p-4 lg:p-12 bg-white m-2 md:m-4 lg:m-0 lg:mt-6 w-[350px] min-w-full xs:w-full sm:max-w-full md:max-w-full mx-auto overflow-x-scroll">
+        <table className="table table-xs table-pin-rows min-w-full overflow-x-scroll table-pin-cols z-0 border-b">
+          <thead>
+            <tr>
+              <th>#</th>
+              <td>Cupon code</td>
+              <td>Discount</td>
+              <td>Description</td>
+              <td>Action</td>
+            </tr>
+          </thead>
+          <tbody>
+            {isloading ? (
+              <h2>Loading......</h2>
+            ) : (
+              cupons?.data?.map((cupon, indx) => (
+                <tr key={cupon._id}>
+                  <th>{indx + 1}</th>
+                  <td>{cupon?.cupon_code}</td>
+                  <td>{cupon?.discount}%</td>
+                  <td>{cupon?.cupon_description.slice(0, 20)}.....</td>
+                  <td>
+                    <button className="btn btn-sm bg-red-600 text-white">
+                      Remove
+                    </button>
+                  </td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </table>
+      </div>
+      <div className="flex items-center justify-center">
+        <button className="btn btn-outline mt-4 mx-auto border-blue-600 text-blue-600 duration-500">Add a cupon</button>
+      </div>
+    </div>
+  );
 };
 
 export default ManageCupons;
